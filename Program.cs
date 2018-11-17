8a0cc9350e7c9d4ba51374e481d649e47af7fb05
Radek <zbychu38@gmail.com>
2018-11-17 17:45:16 +0100

    Multiple child processes

--- Program.cs
+++ Program.cs
@@ -14,21 +14,52 @@ namespace TSST
 
                 List<Client> clients;
                 List<Node> nodes;
-                ManagementCenter managementCenter;
-                CableCloud cableCloud;
 
 
-                Process process = new Process();
-                process.StartInfo.FileName = "TSST.exe";
-                process.StartInfo.Arguments = "-n";
-                process.Start();
+                Process process1 = new Process();
+                process1.StartInfo.FileName = "TSST.exe";
+                process1.StartInfo.Arguments = "-CC";
+                process1.Start();
 
+                Process process2 = new Process();
+                process2.StartInfo.FileName = "TSST.exe";
+                process2.StartInfo.Arguments = "-MC";
+                process2.Start();
+
+                Process process3 = new Process();
+                process3.StartInfo.FileName = "TSST.exe";
+                process3.StartInfo.Arguments = "-N";
+                process3.Start();
+
+                Process process4 = new Process();
+                process4.StartInfo.FileName = "TSST.exe";
+                process4.StartInfo.Arguments = "-C";
+                process4.Start();
 
                 Console.ReadKey();
             }
-            else if (args.Length!=0)
+            else if (args[0] == "-CC")
             {
-                Console.WriteLine("I am Child");
+                CableCloud cableCloud;
+                Console.WriteLine("I am CableCloud");
+                Console.ReadKey();
+            }
+            else if (args[0] == "-MC")
+            {
+                ManagementCenter managementCenter;
+                Console.WriteLine("I am ManagementCenter");
+                Console.ReadKey();
+            }
+            else if (args[0] == "-N")
+            {
+                Node node;
+                Console.WriteLine("I am Node");
+                Console.ReadKey();
+            }
+            else if (args[0] == "-C")
+            {
+                Client client;
+                Console.WriteLine("I am Client");
                 Console.ReadKey();
             }
         }
