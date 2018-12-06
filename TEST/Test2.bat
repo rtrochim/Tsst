5fe3e5418fbdaf6380581b8741140688668f90a0
Radek <zbychu38@gmail.com>
2018-12-06 21:29:47 +0100

    Second test case works

---
+++ TEST/Test2.bat
@@ -0,0 +1,9 @@
+start cmd.exe /c .\CableCloud.exe.lnk ..\..\..\TEST\configs\CableCloud1.conf
+start cmd.exe /c .\Node.exe.lnk ..\..\..\TEST\configs\Node1.conf
+start cmd.exe /c .\Node.exe.lnk ..\..\..\TEST\configs\Node2.conf
+start cmd.exe /c .\Node.exe.lnk ..\..\..\TEST\configs\Node3.conf
+start cmd.exe /c .\Client.exe.lnk ..\..\..\TEST\configs\Client1.conf
+start cmd.exe /c .\Client.exe.lnk ..\..\..\TEST\configs\Client2.conf
+start cmd.exe /c .\Client.exe.lnk ..\..\..\TEST\configs\Client3.conf
+start cmd.exe /c .\ManagementCenter.exe.lnk
+
