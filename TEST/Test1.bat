7a2eccece32fdc07f0388bf844cdb94122a22081
Radek <zbychu38@gmail.com>
2018-12-09 21:00:41 +0100

    Brace yourselves

--- TEST/Test1.bat
+++
@@ -1,8 +0,0 @@
-start cmd.exe /c .\CableCloud.exe.lnk ..\..\..\TEST\configs\CableCloud1.conf -6 600
-start cmd.exe /c .\Node.exe.lnk ..\..\..\TEST\configs\Node1.conf -6 600
-start cmd.exe /c .\Node.exe.lnk ..\..\..\TEST\configs\Node2.conf -6 600
-start cmd.exe /c .\Node.exe.lnk ..\..\..\TEST\configs\Node3.conf -6 600
-start cmd.exe /c .\Client.exe.lnk ..\..\..\TEST\configs\Client1.conf -6 600
-start cmd.exe /c .\Client.exe.lnk ..\..\..\TEST\configs\Client2.conf -6 600
-start cmd.exe /c .\ManagementCenter.exe.lnk -6 600
-
